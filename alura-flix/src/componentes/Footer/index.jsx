6900cb62071c18a58e41fb9bb061289e7964aa8d
mmrc1992 <mmrc1992@gmail.com>
2025-01-19 00:44:19 -0300

    Corregir rutas de imágenes de redes sociales usando rutas relativas

--- alura-flix/src/componentes/Footer/index.jsx
+++ alura-flix/src/componentes/Footer/index.jsx
@@ -13,10 +13,10 @@ function Footer() {
 
             <div className="redes-sociales">
                 <a href="https://www.linkedin.com/in/micaela-roig-courtis/" target="_blank" rel="noopener noreferrer">
-                    <img className="linkedin" src="/linkedin.png" alt="LinkedIn" />
+                    <img className="linkedin" src="linkedin.png" alt="LinkedIn" />
                 </a>
                 <a href="https://github.com/mmrc1992" target="_blank" rel="noopener noreferrer">
-                    <img className="github" src="/github.png" alt="GitHub" />
+                    <img className="github" src="github.png" alt="GitHub" />
                 </a>
             </div>
         </footer>
